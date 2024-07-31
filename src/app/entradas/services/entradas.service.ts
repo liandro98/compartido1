e2a0356fe24a341e41408784e9d4e2e8bffbc0e1
Abigail <gpeabigailcs@gmail.com>
2024-07-31 00:00:51 -0600

    Agregar nuevas carpetas y archivos al proyecto

---
+++ src/app/entradas/services/entradas.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, Pipe,PipeTransform } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError,map,of } from 'rxjs';
+import {Entradas}
+@Injectable({
+  providedIn: 'root'
+})
+export class EntradasService {
+
+  constructor() { }
+}
