a86b6adeb0f7dcb3288274ea1918a763c0f2da98
abigailsalazarvillanueva <gpeabigailcs@gmail.com>
2024-08-18 16:56:22 -0600

    inicio de sesion

--- src/app/entradas/services/usuario.service.ts
+++ src/app/entradas/services/usuario.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from '../interfaces/usuario';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +10,10 @@ export class UserService {
   private apiUrl = 'http://localhost:3000/usuarios';
 
   constructor(private http: HttpClient) {}
+
+  validateUser(usr: string, cont: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl + '/login', { user: usr, contrasena: cont });
+  }
 
   registerUser(usuario: Usuario): Observable<any> {
     return this.http.post<any>(this.apiUrl, usuario);
@@ -27,7 +30,7 @@ export class UserService {
   searchUser(query: { id?: string, name?: string }): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(this.apiUrl + '/search', { params: query });
   }
-  
+
   getAllUsers(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(this.apiUrl + '/all');
   }
